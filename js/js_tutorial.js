47f942fecac97103594eeae391dc4df7162a76fd
weebs11 <kwehbi@gmail.com>
2021-03-06 10:15:34 -0600

    tutorial complete

--- js/js_tutorial.js
+++ js/js_tutorial.js
@@ -274,3 +274,80 @@ END CODE - Functions */
 // The DOM
 
 // START CODE - DOM
+/*
+// Single element selectors
+console.log(document.getElementById('my-form'));
+
+console.log(document.querySelector('h1')); // only will select the first h1
+
+// Multiple element selectors
+
+console.log(document.querySelectorAll('.item'));
+
+const items = document.querySelectorAll('.item');
+
+items.forEach((item) => console.log(item));
+
+const ul = document.querySelector('.items');
+// ul.remove();
+// ul.lastElementChild.remove();
+// ul.firstElementChild.textContent = 'Poop';
+// ul.children[1].innerText = 'Kelly';
+// ul.lastElementChild.innerHTML = '<h1>Big Text</h1>';
+
+// const btn = document.querySelector('.btn');
+// btn.style.background = 'red';
+*/
+
+
+// Events
+
+const btn = document.querySelector('.btn');
+
+/* btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('click');
+    console.log(e.target.className);
+    document.querySelector('#my-form').style.background = '#ccc';
+    document.querySelector('body').classList.add('bg-dark');
+    document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>'
+});
+*/
+
+/*  btn.addEventListener('mouseout', (e) => {
+    e.preventDefault();
+    console.log('click');
+    console.log(e.target.className);
+    document.querySelector('#my-form').style.background = '#ccc';
+    document.querySelector('body').classList.add('bg-dark');
+    document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>'
+});
+*/
+
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+    
+    if(nameInput.value === '' || emailInput.value === '') {
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields';
+        setTimeout(() => msg.remove(), 3000);
+    }
+    else {
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+        
+        userList.appendChild(li);
+
+        // Clear fields
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
